54f6d9744a522ee81c9680d73b5df2b706d5166e
rlaxodus <hbj04003@gmail.com>
2023-04-28 22:09:09 +0900

    Merge branch 'main' of https://github.com/haeguang07/middleProject.git into main

--- checkcheck/src/main/java/com/yedam/common/FrontController.java
+++ checkcheck/src/main/java/com/yedam/common/FrontController.java
@@ -24,6 +24,7 @@ public class FrontController extends HttpServlet{
 	public void init(ServletConfig config) throws ServletException{
 		encoding = config.getInitParameter("enc");
 		map.put("/main.do", new mainControl());
+		map.put("/taeyeon.do", new TaeYeonControl());
 		//상품목록페이지
 		
 	}
