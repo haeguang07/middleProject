f700810e70aa9ea5af49f312c40c0395bfb9f7a7
rlaxodus <hbj04003@gmail.com>
2023-05-03 14:48:06 +0900

    Merge branch 'main' of https://github.com/haeguang07/middleProject.git into main

--- checkcheck/src/main/java/com/yedam/book/mapper/BookMapper.java
+++ checkcheck/src/main/java/com/yedam/book/mapper/BookMapper.java
@@ -1,8 +1,11 @@
 package com.yedam.book.mapper;
 
+import java.util.List;
+
 import com.yedam.book.domain.BookVO;
 
 public interface BookMapper {
 	public int addbook(BookVO vo);
+	public List<BookVO> mainRecommand();
 	
 }
