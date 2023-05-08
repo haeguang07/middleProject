ab8beda51ae79819a8a3985bc68c6d112618479e
rlaxodus <hbj04003@gmail.com>
2023-05-08 17:29:51 +0900

    17:29 0508 yeon

--- checkcheck/src/main/java/com/yedam/book/control/CommentControl.java
+++ checkcheck/src/main/java/com/yedam/book/control/CommentControl.java
@@ -1,19 +1,38 @@
 package com.yedam.book.control;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.yedam.common.Control;
+import com.yedam.review.domain.ReviewVO;
+import com.yedam.review.service.ReviewService;
+import com.yedam.review.service.ReviewServiceImpl;
 
 public class CommentControl implements Control {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		return null;
+		ReviewService service = new ReviewServiceImpl();
+		long isbn = Long.parseLong(req.getParameter("isbn"));
+		List<ReviewVO> list = service.getBookReview(isbn);
+		String json="";
+		Map<String,Object> map = new HashMap<>();
+		if(list != null) {
+			map.put("retCode", "Success");
+		}else {
+			map.put("retCode", "Fail");
+		}
+		Gson gson = new GsonBuilder().create();
+		json = gson.toJson(list);
+		return json+".json";
 	}
 
 }
