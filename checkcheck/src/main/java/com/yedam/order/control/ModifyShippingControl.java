a2b980edcd5d95b7fba868a81aae1a19f1bb4c4a
Haeguang Jeong <hk97564@naver.com>
2023-05-08 12:29:05 +0900

    mypage

--- checkcheck/src/main/java/com/yedam/order/control/ModifyShippingControl.java
+++ checkcheck/src/main/java/com/yedam/order/control/ModifyShippingControl.java
@@ -6,14 +6,33 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.yedam.common.Control;
+import com.yedam.order.domain.OrderVO;
+import com.yedam.order.service.OrderService;
+import com.yedam.order.service.OrderServiceImpl;
 
 public class ModifyShippingControl implements Control {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		OrderVO vo = new OrderVO();
+		vo.setOrderId(req.getParameter("id"));
+		vo.setOrderName(req.getParameter("name")); 
+		vo.setOrderName(req.getParameter("joinPost")); 
+		vo.setOrderName(req.getParameter("joinAdr")+req.getParameter("joinAdr2")); 
+		vo.setOrderName(req.getParameter("phone")); 
+		OrderService service = new OrderServiceImpl();
+		String json="";
+		if(service.orderChange(vo)) {
+			json="{'reqCode':'Success'}";
+			Gson gson =new GsonBuilder().create();
+			json += gson.toJson(vo);
+		}else {
+			json="{'reqCode':'Fail'}";
+		}
+		return json+".json";
 	}
 
 }
